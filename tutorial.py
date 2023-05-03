3bdd07628fd8b209778c15b57b0a5eee25170540
Ankit <arana@Ankits-MacBook-Pro.local>
2023-05-02 17:45:28 -0700

    changes

--- tutorial.py
+++ tutorial.py
@@ -3,3 +3,9 @@ print('hi'+'1231231'+str(99999))
 def a_method():
     print('well here we are in a method')
 print('\n\t all done, now out')
+
+def gcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a % b)
