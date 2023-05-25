3e8b3f1dc2b3c3065eb2350906fd3a7892f09049
Ankit <arana@Ankits-MacBook-Pro.local>
2023-05-24 20:51:35 -0700

    comaprison opr

---
+++ comparison.py
@@ -0,0 +1,17 @@
+x = 3 > 2
+print(x)
+x = 3 == 2
+print(x)
+x = 3 != 2
+print(x)
+
+#logical operators
+price = 22
+print(price > 10 and price <30)
+
+price = 5
+print(price > 10 or price <30)
+
+price = 85
+print(not price > 10)
+
--- math_1.py
+++ math_1.py
@@ -8,3 +8,12 @@ print(12//12)
 print(12%12)
 #exponent
 print(12**12)
+
+x = 10
+x+= 12
+print(x)
+
+y = 1+3/6*4**4
+print(y)
+z = 1+(3/6)*(4**4)
+print(z)
