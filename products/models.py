2b137ba607d148349e2f0ed18861fa83dc15a557
ankitrana <anartikna@gmail.com>
2025-06-16 18:52:44 -0700

    created a model based on whick migration was run and that created tables for our app in the sqlite3

--- products/models.py
+++ products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+#inherit Model by Proucts - allows things like store in a db, read, delete from db on moel objs
+class Product(models.Model):
+    name = models.CharField(max_length = 255)
+    price = models.FloatField()
+    stock = models.IntegerField()
+    image_url = models.CharField(max_length=2083)
+
+
+    
